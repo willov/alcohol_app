df00f4e8243c88beb668eecc7b1d0be9f4424098
Henrik Podéus Derelöv <henrik.podeus.derelov@liu.se>
2025-10-27 16:41:29 +0100

    started working on the paper 2 demo

--- pages/09_Alcohol secondary metabolities dynamics copy.py
+++ pages/09_Alcohol secondary metabolities dynamics copy.py
@@ -36,7 +36,7 @@ def setup_model(model_name):
     features = model.feature_names
     return model, features
 
-model, model_features = setup_model('alcohol_model')
+model, model_features = setup_model('alcohol_model_2')
 
 # Define functions needed
 
@@ -67,14 +67,11 @@ def simulate(m, anthropometrics, stim, extra_time = 10):
 
 # Start the app
 
-st.markdown("# Alcohol dynamics")
-st.write("Simulation of alcohol dynamics")
-st.markdown("""Current models for alcohol dynamics lack detailed dynamics of e.g., gastric emptying. 
-We have constructed a more detailed model able to both explain differing dynamics of different types of drinks, as well as differences based on anthropometrics. 
-Using the model, you can simulate the dynamics of different drinks based on custom anthropometrics. 
+st.markdown("# Secondary alcohol metabolites")
+st.write("Simulation of secondary alcohol metabolites")
+st.markdown("""We have constructed a more detailed mechanistic model that involves secondary metabolites, ethyl glucuronide (EtG), ethyl sulphate (EtS), urine alcohol concentration (UAC), able to both explain the profiles of these metabolites following consumption of alcohol. 
 
-Below, you can specify one or more alcoholic drinks, and some anthropometrics to get simulations of the ethanol concentration in blood as well as clinical markers of alcohol use like phosphatidylethanol (PEth).  
-
+Below, you can specify one or more alcoholic drinks, and some anthropometrics to get simulations of the concentration of these metabolites in plasma.
 """)
 
 # Anthropometrics            
@@ -87,11 +84,14 @@ if 'weight' not in st.session_state:
     st.session_state['weight'] = 70.0
 if 'height' not in st.session_state:
     st.session_state['height'] = 1.72
+if 'age' not in st.session_state:
+    st.session_state['age'] = 30
 
-anthropometrics = {"sex": st.session_state['sex'], "weight": st.session_state['weight'], "height": st.session_state['height']}
+anthropometrics = {"sex": st.session_state['sex'], "weight": st.session_state['weight'], "height": st.session_state['height'], "age": st.session_state['age']}
 anthropometrics["sex"] = st.selectbox("Sex:", ["Man", "Woman"], ["Man", "Woman"].index(st.session_state['sex']), key="sex")
 anthropometrics["weight"] = st.number_input("Weight (kg):", 0.0, 1000.0, st.session_state.weight, 0.1, key="weight")
 anthropometrics["height"] = st.number_input("Height (m):", 0.0, 2.5, st.session_state.height, key="height")
+anthropometrics["age"] = st.number_input("Age (years):", 0, 120, st.session_state.age, key="age")
 
 anthropometrics["sex"] = float(anthropometrics["sex"].lower() in ["male", "man", "men", "boy", "1", "chap", "guy"]) #Converts to a numerical representation
 
