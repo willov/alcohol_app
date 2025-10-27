148848e97446e4d450e702db72762b4f369744a2
William Lövfors <william.lovfors@liu.se>
2025-10-27 12:07:06 +0100

    Update lingering reference to biorxiv

--- Home.py
+++ Home.py
@@ -25,13 +25,11 @@ We hope that this model and the applications can be used to make more informed d
 
 Please note that this application is only for research and visualization purposes, and should not be used to make medical decisions. 
             
-This application is a companion-application to the publication titled \"A physiological-twin for alcohol consumption: connecting short-term drinking habits to plasma PEth\", [available as a preprint on bioRxiv](https://www.biorxiv.org/content/10.1101/2023.08.18.553836). 
+This application is a companion-application to the publication \"*A physiologically-based digital twin for alcohol consumption — predicting real-life drinking responses and long-term plasma PEth*\", [published in NPJ digital medicine](https://doi.org/10.1038/s41746-024-01089-6). 
             
 If you use this application, please cite as: 
 
-> A Physiologically Based Digital Twin for Alcohol Consumption – Predicting Real-life Drinking Responses and Long-term Plasma PEth.
-Henrik Podéus, Christian Simonsson, Patrik Nasr, Mattias Ekstedt, Stergios Kechagias, Peter Lundberg, William Lövfors, Gunnar Cedersund.
-bioRxiv 2023.08.18.553836; doi: https://doi.org/10.1101/2023.08.18.553836 
+> Podéus H, Simonsson C, Nasr P, Ekstedt M, Kechagias S, Lundberg P, Lövfors W, Cedersund G (2024) A physiologically-based digital twin for alcohol consumption — predicting real-life drinking responses and long-term plasma PEth. npj Digital Medicine 7:112. https://doi.org/10.1038/s41746-024-01089-6
 
 """)
 
